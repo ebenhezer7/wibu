05b6a0ddb17cd824041da4492fb7112bd089a63c
ebenhezer7 <tersakamdo80@gmail.com>
2022-12-16 19:08:28 +0700

    rencana

---
+++ search.php
@@ -0,0 +1,10 @@
+<form action="" method="post">
+    <<div class="form-group">
+        <div class="input-grup">
+            <input type="text" name="cari" class="form-control">
+            <<div class="input-group-prepend">
+                <button type="submit" class="btn"><<i class="fa fa-search" aria-hidden="true"></i></button>
+            </div>
+        </div>
+    </div>
+</form>
